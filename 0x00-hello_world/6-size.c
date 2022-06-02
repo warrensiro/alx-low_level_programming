08c3f197ebe92f9387574786e2bdabd36cfce66b
warrensiro <warrensiro@gmail.com>
2022-06-02 16:05:51 -0700

    6th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - entry point to call a function
- *
- * description - to print the size of the variable using printf
- *
- * Return:success
+*main - Program starting point
+*
+* Description - Displaying sizeof data types
+*
+* Return: Always 0 Succes
 */
+
 int main(void)
 {
 	char acharacter;
@@ -15,10 +17,11 @@ int main(void)
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of a int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return (0);
 }
+
