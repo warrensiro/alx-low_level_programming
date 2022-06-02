608ef1e9e188a348fd4c4439a03c1ff3e40ae0ef
warrensiro <warrensiro@gmail.com>
2022-06-02 15:08:02 -0700

    second syntax test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point to call a function
+ *
+ * description - prints the statement using printf function
+ *
+ * Return: success
+*/
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art\, \);
+	return (0);
+}
