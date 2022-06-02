09c6d3cccca27892c72151c530db2dba3901e62f
warrensiro <warrensiro@gmail.com>
2022-06-02 14:53:21 -0700

    C syntax

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+*main - entry point of program
+*
+*description - Program to print output using puts
+*
+*Return: Always 0 Succes
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
