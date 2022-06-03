d03abb402f745b8ea398cf6c86eae03f84900186
warrensiro <warrensiro@gmail.com>
2022-06-03 13:53:11 -0700

    o commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* can contain more headers*/
+#include <stdio.h>
+/*
+ * main - used to call the function
+ *
+ * Description - to check and return result of the function
+ *
+ * Return: success
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(o));
+	n=rand() - RAND_MAX / 2;
+	/*your code goes in there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
