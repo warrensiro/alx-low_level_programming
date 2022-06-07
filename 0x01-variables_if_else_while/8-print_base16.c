d96373377623d3959019a5c53b5a08d46b432d0b
warrensiro <warrensiro@gmail.com>
2022-06-07 00:36:10 -0700

    8th commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point of a program
+ *
+ * description - print numbers of base 16 in lowercase
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	int digit;
+
+	char letter;
+
+	for (digit = o; digit <= 9; digit++)
+		putchar((digit % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
