aebeaf7eba036e3b0e030be6ef668d716ab7da2c
warrensiro <warrensiro@gmail.com>
2022-06-07 12:44:07 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point of a program
+ *
+ * description - print a string
+ *
+ * Return: always 0 success
+*/
+
+int main(void)
+{
+	char *m = _putchar;
+	while(*m)
+	{
+		_putchar(*m);
+		m++;
+	}
+	_putchar("\n");
+
+	return (0);
+}
