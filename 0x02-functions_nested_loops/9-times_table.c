b6885ce689366f7580204b43e7eb82ceb5308490
warrensiro <warrensiro@gmail.com>
2022-06-08 16:42:37 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2+++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
 			prod = num1 * num2;
 
@@ -30,7 +30,7 @@ void times_table(void)
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
-			if (num2 I= 9)
+			if (num2 != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
