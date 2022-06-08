c617da5c2b53d9248df12e6a80f14299d568b9b8
warrensiro <warrensiro@gmail.com>
2022-06-08 13:32:56 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point of a program
+ *
+ * description - to print the alphabet
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+
+	return (0);
+}
