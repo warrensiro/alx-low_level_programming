cab4d538ae4b997989600d549c97cbccaf90f1df
warrensiro <warrensiro@gmail.com>
2022-06-08 16:18:09 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - check code to call for time
+ *
+ * description - count the time
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
