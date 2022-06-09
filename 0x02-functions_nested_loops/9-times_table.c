3b7995605a9e84181ce9d4cc2657340b8bc54a5b
warrensiro <warrensiro@gmail.com>
2022-06-08 18:36:24 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,10 +18,10 @@ void times_table(void)
 
 			if (prod == 0)
 			{
-				while (num2 > 0)
+				_putchar((prod % 10) + '0');
+				while(num2 > 0)
 				{
 					_putchar(' ');
-					_putchar((prod % 10) + '0');
 				}
 			}
 			else if (prod >= 1 && prod <= 9)
