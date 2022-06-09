e1acd13d126701bb2740d840eec32a858a48cda4
warrensiro <warrensiro@gmail.com>
2022-06-09 13:11:03 -0700

    more nesting

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code
+ * @c: call in the integer
+ *
+ * Return: 1 if success otherwise all lowercase
+ */
+
+int _isupper(int c)
+{
+	char c;
+
+	for ((c >= 'A') && (c <= 'Z'))
+	if (c = 'A')
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	else
+	{
+		printf("%c: %d\n", c, _isupper(c));
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+deepdive to nesting
