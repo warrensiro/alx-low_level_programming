950ff57a4d562eb8aebf993f7035c7f2f498aa57
warrensiro <warrensiro@gmail.com>
2022-06-08 17:04:18 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - function to give a summation
+ *
+ * @m: calls the first variable
+ *
+ * @n: calls the second variable
+ *
+ * Return: add the two variables and give to the program
+ */
+int add(int m, int n)
+{
+	int results;
+
+	result = a + b;
+
+	return (result);
+}
