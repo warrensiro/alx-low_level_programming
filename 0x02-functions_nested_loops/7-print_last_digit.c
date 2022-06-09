6e5d0aa1eb86fa111d6a32ac597602eccb7469ad
warrensiro <warrensiro@gmail.com>
2022-06-08 17:23:07 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,19 @@
 /**
  * print_last_digit - check code
  *
- * @m: function for modulus
+ * @l: function for modulus
  *
  * Return: bring the result
  */
-int print_last_digit(int m)
+int print_last_digit(int l)
 {
 	int ret;
 
-	ret = m % 10;
+	ret = l % 10;
 
 	if (ret < 0)
 	{
-		ret = (-m * ret);
+		ret = (-l * ret);
 	}
 	_putchar(ret + '0');
 	return (ret);
