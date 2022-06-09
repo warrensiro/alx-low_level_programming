4f227420458d64eba59620e5debbf986339237e5
warrensiro <warrensiro@gmail.com>
2022-06-09 02:27:05 -0700

    testing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,18 @@
 
 /**
  * main- tests function that prints if integer is positive or negative
+ * @i: function to check if positive or negative
+ *
  * Return: 0
+ *
  */
 
-int main(void)
+void positive_or_negative(int i)
 {
 	int i;
 
 	i = 98;
-	{
-		positive_or_negative(i);
-	}
+	positive_or_negative(i);
+	
 	return (0);
 }
