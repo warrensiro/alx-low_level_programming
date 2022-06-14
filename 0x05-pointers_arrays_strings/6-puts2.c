d0fba66500631b715cb73d32ca3b45988e998d81
warrensiro <warrensiro@gmail.com>
2022-06-14 14:31:07 -0700

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - to print starting with first character
+ * @str: pointer to the string
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int z = 0;
+	int y = 0;
+	char *c = str;
+	int x;
+
+	while (*c != '\0')
+	{
+		c++;
+		z++;
+	}
+	y = z - 1;
+
+	for (x = 0; x <= y; x++)
+	{
+		if ( x % 2 == 0)
+		{
+			_putchar(str[d]);
+		}
+	}
+	_putchar('\0');
+}
