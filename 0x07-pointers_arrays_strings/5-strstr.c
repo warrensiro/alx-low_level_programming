5cf2d37f16687e70acc0b98b6260fd5514c2afde
warrensiro <warrensiro@gmail.com>
2022-06-21 15:00:18 -0700

    locate a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * strstr - function to cal a substring
+ * @haystack: first string, the one to be searched
+ * @needle: second string, the one to be found
+ *
+ * Return: NULL
+ */
+char *strstr(char *haystack, char *needle)
+{
+	int z = 0;
+	int y = 0;
+
+	while (haystack[z])
+	{
+		while (needle[y])
+		{
+			if (haystack[z + y] != needle[y])
+			{
+				break;
+			}
+			y++;
+		}
+		if (needle[y] == '\0')
+		{
+			return (haystack +a);
+		}
+		z++;
+	}
+	return ('\0');
+}
