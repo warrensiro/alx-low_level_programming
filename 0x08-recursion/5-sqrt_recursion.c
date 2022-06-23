68d7f7cfb5e2cc6d1373deda43db9fbd5b4750d8
warrensiro <warrensiro@gmail.com>
2022-06-23 03:31:41 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * sqrt_a - check code
+ * @y: number
+ * @z: number of root
+ *
+ * Return: always success
+ */
+int sqrt_a(int y, int z)
+{
+	if (z * z == y)
+	{
+		return (z);
+	}
+	else if (z * z > y)
+	{
+		retur (-1);
+	}
+	else
+	{
+		return (sqrt_a(y, z + 1));
+	}
+}
+/**
+ * _sqrt_recursion - check code
+ * @n: the integer
+ *
+ * Return: always success
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_a(n, 0));
+}
