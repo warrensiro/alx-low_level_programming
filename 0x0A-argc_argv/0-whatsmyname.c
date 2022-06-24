2a79011f7a5636ebd37a8e61b5a2287af3a524c1
warrensiro <warrensiro@gmail.com>
2022-06-24 12:52:09 -0700

    same output without compiling

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check code
+ * @argc: the number of command line entries
+ * @argv: the array reffered to by argc
+ *
+ * Return: always 0
+ */ 
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
