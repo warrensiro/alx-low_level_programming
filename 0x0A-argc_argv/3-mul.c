f49e56ac8b513ea3eb3c9a6ee5e448168169eef2
warrensiro <warrensiro@gmail.com>
2022-06-24 14:29:08 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
- * main - check code
- * @argc: the number of command line argv
- * @argv: array being reffered to by argc
+ * main - print code for two numbers
+ * @argc: number of command line arguments
+ * @argv: array of arguments reffered to by argc
  *
- * Return: always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int num;
-	if (argc == 3)
+	if (argc <= 2 || argc > 3)
 	{
-		num *= atoi(argv[argc - 2]);
-		num *= atoi(argv[argc - 1]);
-		printf("%d\n", num);
+		printf("syntax\n");
+		return (1);
 	}
 	else
 	{
-		printf("Error\n");
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-	return (0);
 }
