aa26ca18af5b9e75b9bc86d0ecb0d122ee4809c2
warrensiro <warrensiro@gmail.com>
2022-07-05 10:38:29 -0700

    struct of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,12 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef fpr dog structure
+ */
+typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
