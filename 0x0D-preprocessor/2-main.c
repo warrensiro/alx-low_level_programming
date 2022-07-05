3c4233449b5f956d3c294dd6d877254ba965593d
warrensiro <warrensiro@gmail.com>
2022-07-05 09:02:09 -0700

    file origin

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - code entry point to print name of file it was compiled from
+ * 
+ * Return: Always success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
