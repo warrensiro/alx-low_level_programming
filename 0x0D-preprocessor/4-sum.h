8a51d9538564a090c72e2de1fcd522ceca398d2f
warrensiro <warrensiro@gmail.com>
2022-07-05 09:29:19 -0700

    sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,15 +1,6 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-/**
- * main - check code for sum of two numbers
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int s;
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
 
-	s = SUM(98, 1024);
-	printf("%d\n", s);
-	return (0);
-}
+#define SUM(x, y) (x + y)
+
+#endif
