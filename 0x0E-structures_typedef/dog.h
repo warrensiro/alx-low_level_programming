41b373379c21e4c79f4e45dde1b5ff95024682c8
warrensiro <warrensiro@gmail.com>
2022-07-05 09:59:24 -0700

    dogs, attributes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - the attributes of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: dog's attributes
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
