d9c5685b27793ff56d2c3359157a0cbac4b7f00e
warrensiro <warrensiro@gmail.com>
2022-07-07 15:47:30 -0700

    change in variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/**
+ * struct format - ...
+ * @op: ...
+ * @f: ...
+ */
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
 #endif
