e77fad90f7140094aec91cf91dad36eb868b61fb
warrensiro <warrensiro@gmail.com>
2022-07-14 14:11:46 -0700

    print elements of a list_t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * print_list - check code
+ * @h: the lists name
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int count = 0;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct lists_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: point to the following node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}
+list_t;
 
 size_t print_list(const list_t *h);
 
