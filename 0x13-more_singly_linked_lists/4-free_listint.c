0680376592be88b7cece2dd1ff15c54a6b4d8acd
warrensiro <warrensiro@gmail.com>
2022-07-19 13:38:19 -0700

    free memory allocation

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * free_listint - free memory allocation in this list
+ * @head: the list
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head);
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
