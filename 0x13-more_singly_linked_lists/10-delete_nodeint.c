18729b116eabe90b6e4e1f59b024e73cb1443dc1
warrensiro <warrensiro@gmail.com>
2022-07-19 15:21:56 -0700

    dlete node at index index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int z = 0;
-	listint_t *tmp_node = *head;	
+	listint_t *tmp_node = *head;
 	listint_t *after_node;
 
 	if (*head == NULL)
