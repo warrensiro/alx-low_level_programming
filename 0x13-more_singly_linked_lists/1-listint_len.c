2a0179abebf5978f90c3773ef588cb5384639c50
warrensiro <warrensiro@gmail.com>
2022-07-19 08:56:29 -0700

    elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * listint _len - check code to validate elements
+ * @h: name of the list
+ *
+ * Return: success
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (h)
+	{
+		return (1 + listint_len(h->next));
+	}
+	return (0);
+}
