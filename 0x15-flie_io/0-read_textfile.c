bcb9b822d80a25a4b519e46678b2b3b9947f5fd5
warrensiro <warrensiro@gmail.com>
2022-07-25 19:19:52 -0700

    read and print to posix standard output

---
+++ 0x15-flie_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile - function to read and output a text file
+ * @filename: source file
+ * @letters: number of letters to read and print
+ *
+ * Return: the filename output
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
+
+	if (!buff)
+	{
+		return (0);
+	}
+	if (!filename)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+
+	free(buff);
+	close(fd);
+	return (readed);
+}
---
+++ 0x15-flie_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _FILE_IO_
+#define _FILE_IO_
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
